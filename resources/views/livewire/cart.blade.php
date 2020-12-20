27f3341dc404e8b33e50a3049a811dd39d1ee654
iyanrspian <iyanrispian@outlook.com>
2020-12-20 10:17:05 +0700

    styling mdbootstrap & SPA

--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -1,28 +1,30 @@
 <div>
     <div class="row">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-body">
+            <div class="card shadow-1-strong">
+                <div class="card-header">
                     <div class="row">
-                        <div class="col-md-8">
+                        <div class="col-md-9">
                             <h5 class="font-weight-bold mt-1">Product List</h5>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-3">
                             <input wire:model="search" type="text" class="form-control form-control-sm" placeholder="Search..">
                         </div>
                     </div>
+                </div>
+                <div class="card-body pb-0">
                     <div class="row">
                         @forelse ($product as $products)
                             <div class="col-md-3 mb-3">
-                                <div class="card">
+                                <div class="card shadow-1-strong">
+                                    <img src="{{ asset('storage/images/'.$products->image) }}" alt="Product" style="object-fit: cover; width: 100%; height: 120px">
+                                    <button wire:click="addItem({{ $products->id }})" class="btn btn-sm btn-primary btn-sm" style="position: absolute; top:8px; right:8px; padding: 8px"><i class="fas fa-cart-plus fa-lg"></i></button>
                                     <div class="card-body">
-                                        <img src="{{ asset('storage/images/'.$products->image) }}" alt="Product" style="object-fit:cover;width:100%;height:80px">
-                                    </div>
-                                    <div class="card-footer">
-                                        <h6 class="font-weight-bold">{{ $products->name }}</h6>
-                                        <h6>{{ $products->desc }}</h6>
-                                        <h6>Rp. {{ number_format($products->price) }}</h6>
-                                        <button wire:click="addItem({{ $products->id }})" class="btn btn-sm btn-primary btn-sm btn-block">Add to Cart</button>
+                                        <h6 class="card-title text-dark" style="font-size: 14pt">{{ $products->name }}</h6>
+                                        <p class="card-text" style="font-size: 12pt">
+                                            {{ $products->desc }} <br>
+                                            <strong class="text-primary">Rp. {{ number_format($products->price) }}</strong>
+                                        </p>
                                     </div>
                                 </div>
                             </div>
@@ -39,7 +41,7 @@
             </div>
         </div>
         <div class="col-md-4">
-            <div class="card">
+            <div class="card shadow-1-strong">
                 <div class="card-body">
                     <h5 class="font-weight-bold">Cart</h5>
                     <p class="text-danger font-weight-bold">
@@ -47,30 +49,32 @@
                             {{ session('error') }}
                         @endif
                     </p>
-                    <table class="table table-sm table-borderless table-striped">
+                    <table class="table table-responsive-sm table-sm table-borderless table-striped">
                         <thead class="bg-primary text-white">
                             <tr>
-                                <th>No</th>
-                                <th>Name</th>
-                                <th>Qty</th>
-                                <th>Total (Rp.)</th>
-                                <th>Ket.</th>
+                                <th>#</th>
+                                <th style="width: 70%">Name</th>
+                                <th class="text-center" style="width: 30%">Qty</th>
+                                <th>Total</th>
                             </tr>
                         </thead>
                         <tbody>
                             @forelse ($cart as $index=>$carts)
                                 <tr>
-                                    <td class="text-center">{{ $index+1 }}</td>
-                                    <td>{{ $carts['name'] }}</td>
+                                    <th scope="row">
+                                        {{ $index+1 }}<br>
+                                        <span wire:click="removeItem('{{ $carts['rowId'] }}')" class="text-dark" style="cursor:pointer"><i class="fas fa-trash fa-sm"></i></span>
+                                    </th>
+                                    <td style="width: 60%">
+                                        {{ $carts['name'] }}<br>
+                                        Rp {{ number_format($carts['price']) }}
+                                    </td>
                                     <td class="text-center">
                                         <span wire:click="decreaseItem('{{ $carts['rowId'] }}')" class="text-danger" style="cursor:pointer"><i class="fas fa-minus-square fa-sm"></i></span>
                                         {{ $carts['qty'] }}
                                         <span wire:click="increaseItem('{{ $carts['rowId'] }}')" class="text-success" style="cursor:pointer"><i class="fas fa-plus-square fa-sm"></i></span>
                                     </td>
                                     <td class="text-right">{{ number_format($carts['prices']) }}</td>
-                                    <td class="text-center">
-                                        <span wire:click="removeItem('{{ $carts['rowId'] }}')" class="text-dark" style="cursor:pointer"><i class="fas fa-trash fa-sm"></i></span>
-                                    </td>
                                 </tr>
                             @empty
                                 <td colspan="5"><h6 class="text-center mt-2">Empty Cart</h6></td>
@@ -79,7 +83,7 @@
                     </table>
                 </div>
             </div>
-            <div class="card mt-3">
+            <div class="card shadow-1-strong mt-3">
                 <div class="card-body">
                     <h5 class="font-weight-bold mb-3">Cart Summary</h5>
                     <table class="table table-sm">
@@ -101,14 +105,14 @@
                     </table>
                     <div class="row mt-3">
                         <div class="col-md-6">
-                            <button wire:click="enableTax" class="btn btn-sm btn-warning btn-block">Add Tax</button>
-                        </div>
-                        <div class="col-md-6">
                             <button wire:click="disableTax" class="btn btn-sm btn-danger btn-block">Remove Tax</button>
                         </div>
+                        <div class="col-md-6">
+                            <button wire:click="enableTax" class="btn btn-sm btn-primary btn-block">Add Tax</button>
+                        </div>
                     </div>
-                    <div class="mt-3">
-                        <button class="btn btn-sm active btn-success btn-block">Save Transaction</button>
+                    <div class="mt-3 mb-1">
+                        <button class="btn btn-sm active btn-success btn-block"><i class="fas fa-save"></i>&nbsp;&nbsp;&nbsp;Save Transaction</button>
                     </div>
                 </div>
             </div>
